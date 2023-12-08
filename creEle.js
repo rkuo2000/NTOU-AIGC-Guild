704dfc74d24e297915c431fc36201a20bcc88e9a
Richard Kuo <rkuo2000@gmail.com>
2023-12-08 23:57:09 +0800

    Add files via upload

---
+++ creEle.js
@@ -0,0 +1,35 @@
+class creEle{
+    constructor(tag = "div"){
+        this.el = document.createElement(tag);
+    }
+    setText(context = ''){
+        const textNode = document.createTextNode(context);
+        if (context === "") {
+          this.el.innerText = "";
+          return;
+        }
+        this.el.append(textNode);
+    }
+    setAttribute(attribute = {}){
+        if (typeof attribute !== "object") return;
+        for (let attr in attribute) {
+            if(attr === 'style'){
+              this.el.style.cssText += attribute[attr];
+            }else{
+              this.el.setAttribute(attr, attribute[attr]);
+            }
+        }
+    }
+}
+
+const aTag = document.createElement('a');
+const imgTag = document.createElement('img');
+aTag.setAttribute('href', 'https://www.facebook.com/groups/aigcguild.ntou');
+aTag.setAttribute('target', '_blank');
+imgTag.setAttribute('src', '圖片網址連結');
+aTag.appendChild(imgTag);
+document.querySelector("body").appendChild(aTag);
+
+const h1 = new creEle('h1');
+h1.setText('我在生成式AI冒險者公會等你，來跟我一起進化吧~');
+document.querySelector("body").appendChild(h1.el)
